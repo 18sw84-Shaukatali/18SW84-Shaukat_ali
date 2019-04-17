46f3a6be11f26549c4f67463c03225595285fe57
18sw84-Shaukatali <49730363+18sw84-Shaukatali@users.noreply.github.com>
2019-04-18 00:32:32 +0500

    Add files via upload

---
+++ PR-01 Tasks/pr01task01.java
@@ -0,0 +1,13 @@
+class Variables {
+	public static void main(String args[]){
+	String name="Shaukat";
+	String rollno="18sw84";
+	short marks=383;
+	double percentage=77.8;
+	String grade="B-";
+	String status="Pass";
+	System.out.println("Name,Rollno,Marks,Percentage,grade, are arranged respectively.");
+	System.out.println(name+" "+rollno+" "+marks+" "+percentage+"% "+grade+" "+status);
+}
+}
+
---
+++ PR-01 Tasks/pr01task02.java
@@ -0,0 +1,8 @@
+class Display {
+	public static void main(String args[]){
+	double a=50.25;
+	long b=50;
+	double c=a-b;
+	System.out.println(a+" "+b+" "+c);
+}
+}
