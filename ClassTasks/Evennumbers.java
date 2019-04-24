ef5ba1859093d1a442e88481a1226876562f63d1
18sw84-Shaukatali <49730363+18sw84-Shaukatali@users.noreply.github.com>
2019-04-25 00:42:38 +0500

    Add files via upload

---
+++ ClassTasks/Evennumbers.java
@@ -0,0 +1,12 @@
+class MainClass {   
+  public static void main(String[] args) { 
+ 
+    // print even numbers between 0 and 100 
+    for(int i = 0; i <= 100; i++) {  
+      if((i%2) != 0) 
+         continue; // iterate
+      System.out.println("Even numbers:"+i); 
+    } 
+ 
+  }   
+}
